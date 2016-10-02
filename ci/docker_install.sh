ac7d475020541bb7f0dfde281b72b400d7e0ccb8
Kushal <kushaldeveloper@gmail.com>
2016-10-02 12:02:09 -0400

    remove old comments that don't apply

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -5,11 +5,9 @@
 
 set -xe
 
-# Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
 apt-get upgrade -yqq
 apt-get install git -yqq
-#install dotnet 
 apt-get install curl libunwind8 gettext libunwind8 libicu52 -yqq
 curl -sSL -o dotnet.tar.gz https://go.microsoft.com/fwlink/?LinkID=827530
 mkdir -p /opt/dotnet && tar zxf dotnet.tar.gz -C /opt/dotnet
